f3bb6b8c9170cccc61d083b66db88cc677765708
YeiserDeveloper <yeiser192@gmail.com>
2022-04-07 08:10:32 -0400

    Adding updating

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,12 @@
-const url = `https://api.adviceslip.com/advice`;
-const btnAdvice = document.getElementById('btnGenerator');
+let fetchAdvice = async () => {
+    let res = await fetch("https://api.adviceslip.com/advice");
+    let data = await res.json();
 
-function random(min, max) {
-    return Math.floor((Math.random() * (max - min + 1)) + min);
+    let title = document.getElementById("title");
+    title.innerHTML = `Advice #${data.slip.id}`;
+    let advice = document.getElementById("advice");
+    advice.innerHTML = `"${data.slip.advice}"`;
 }
 
-let numberRandom = random(1, 200)
-
-async function getData() {
-    const dataId = document.getElementById('numberID')
-    const dataText = document.getElementById('advice')
-    const response = await fetch(`${url}/${numberRandom}`);
-    const data = await response.json();
-    const getId = data.slip.id;
-    const getText = data.slip.advice;
-    dataId.innerHTML = getId;
-    dataText.innerHTML = getText;
-    return advice;
-    getData();
-}
-
-btnAdvice.addEventListener('click', getData())
+let btn = document.querySelector('.btn');
+btn.addEventListener('click', fetchAdvice);
